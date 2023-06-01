52768081832db58d42057c1dbacb885c8a3cee96
nicoleonard <nikolasleonard@gmail.com>
2023-06-01 02:30:05 -0300

    agregado main con titulo y contenedores para el formulario y las cards, agregado link de style en intex.html, agregado footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
 
 function App() {
   return (
     <>
+      <main className='w-100 m-0'>
+        <Container className='text-start text-dark-emphasis py-5'>
+          <h1>Administrador de pacientes de veterinaria</h1>
+          <hr />
+        </Container>
+        <Container>
+          
+        </Container>
+        <Container>
+
+        </Container>
+      </main>
+      <footer className='bg-dark bg-gradient text-center text-light py-5 w-100'>
+        <p>&copy; Todos los derechos reservados</p>
+      </footer>
     </>
   )
 }
